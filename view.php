fa40f0542204ed6f923cd3ccad357e7ed277ac3d
tusenschandrasegarran <66305417+tusenschandrasegarran@users.noreply.github.com>
2020-06-11 14:42:47 +0800

    Update view.php

--- view.php
+++ view.php
@@ -6,6 +6,21 @@
 if(!$conn){
   die("Failed to connect");
 }
-echo "Connection Success";
-
+//echo "Connection Success";
+  $tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+{
+die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>". $row['restaurant_id'] . "</td>";
+echo "<td>". $row['restaurant_name'] ."</td>";
+echo "<td>". $row['restaurant_address'] . "</td>";
+echo "<td>". $row['restaurant_phone'] . "</td>";
+echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
 ?>
